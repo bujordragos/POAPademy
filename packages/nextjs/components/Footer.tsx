e63f01d8c9e295a27f38c826cadbfe0c8d9d3dbc
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-04-03 17:17:14 +0300

    add social media icons

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 import { hardhat } from "viem/chains";
 import { SwitchTheme } from "~~/components/SwitchTheme";
 import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
@@ -10,44 +12,53 @@ export const Footer = () => {
   const { targetNetwork } = useTargetNetwork();
   const isLocalNetwork = targetNetwork.id === hardhat.id;
 
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0">
       <div>
         <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
           <div className="flex flex-col md:flex-row gap-2 pointer-events-auto">
             {/* Social media links */}
-            <div className="flex gap-3">
+            <div className="flex gap-6">
               <a
                 href="https://www.instagram.com/poap.ademy5/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-white hover:underline"
+                className={resolvedTheme === "dark" ? "text-white hover:underline" : "text-[#151926] hover:underline"}
               >
-                Instagram
+                <FaInstagram size={24} />
               </a>
               <a
                 href="https://web.facebook.com/profile.php?id=61574989048352"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-white hover:underline"
+                className={resolvedTheme === "dark" ? "text-white hover:underline" : "text-[#151926] hover:underline"}
               >
-                Facebook
+                <FaFacebook size={24} />
               </a>
               <a
                 href="https://x.com/poapademy16618"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-white hover:underline"
+                className={resolvedTheme === "dark" ? "text-white hover:underline" : "text-[#151926] hover:underline"}
               >
-                X (Formerly Twitter)
+                <FaTwitter size={24} />
               </a>
               <a
                 href="https://www.linkedin.com/in/poapademy-company-69427a35a/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-white hover:underline"
+                className={resolvedTheme === "dark" ? "text-white hover:underline" : "text-[#151926] hover:underline"}
               >
-                LinkedIn
+                <FaLinkedin size={24} />
               </a>
             </div>
           </div>
