e8def023507a255d8417d7f2fe626f5e3de1239b
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-04-03 16:38:00 +0300

    add react-icons

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -14,7 +14,43 @@ export const Footer = () => {
     <div className="min-h-0 py-5 px-1 mb-11 lg:mb-0">
       <div>
         <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
-          <div className="flex flex-col md:flex-row gap-2 pointer-events-auto"></div>
+          <div className="flex flex-col md:flex-row gap-2 pointer-events-auto">
+            {/* Social media links */}
+            <div className="flex gap-3">
+              <a
+                href="https://www.instagram.com/poap.ademy5/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:underline"
+              >
+                Instagram
+              </a>
+              <a
+                href="https://web.facebook.com/profile.php?id=61574989048352"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:underline"
+              >
+                Facebook
+              </a>
+              <a
+                href="https://x.com/poapademy16618"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:underline"
+              >
+                X (Formerly Twitter)
+              </a>
+              <a
+                href="https://www.linkedin.com/in/poapademy-company-69427a35a/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white hover:underline"
+              >
+                LinkedIn
+              </a>
+            </div>
+          </div>
           <SwitchTheme className={`pointer-events-auto ${isLocalNetwork ? "self-end md:self-auto" : ""}`} />
         </div>
       </div>
