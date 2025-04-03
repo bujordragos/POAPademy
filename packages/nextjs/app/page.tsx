5e83c3e3de613bda2bbd8416dd547c2ebf0749be
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-04-03 17:58:35 +0300

    add main page new layout

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import type { NextPage } from "next";
 // import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { AcademicCapIcon, BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 // import { Address } from "~~/components/scaffold-eth";
 
@@ -12,18 +12,20 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">
+      <div className="flex items-center flex-col flex-grow mt-16">
+        {" "}
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">POAPademy</span>
+            <span className="block text-2xl mb-4">Welcome to</span>
+            <span className="block text-4xl font-bold mb-14">POAPademy</span>
           </h1>
         </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col md:flex-row">
+        <div className="flex-grow bg-base-300 w-full px-8 py-1 pt-20">
+          {" "}
+          <div className="flex justify-center items-center gap-24 flex-col md:flex-row">
+            {" "}
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
+              <BugAntIcon className="h-8 w-8 fill-secondary mb-4" />
               <p>
                 Tinker with your smart contract using the{" "}
                 <Link href="/debug" passHref className="link">
@@ -33,7 +35,17 @@ const Home: NextPage = () => {
               </p>
             </div>
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
+              <AcademicCapIcon className="h-8 w-8 fill-secondary mb-4" />
+              <p>
+                Explore your local transactions with the{" "}
+                <Link href="/blockexplorer" passHref className="link">
+                  Learning
+                </Link>{" "}
+                tab.
+              </p>
+            </div>
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary mb-4" />
               <p>
                 Explore your local transactions with the{" "}
                 <Link href="/blockexplorer" passHref className="link">
