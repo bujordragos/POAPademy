ce75e67eb7cb71c449f009eaf501ffa37d22151d
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2025-04-05 02:31:52 +0300

    change motto

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -119,7 +119,7 @@ export const Header = () => {
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">POAPADEMY</span>
-            <span className="text-xs">Learn. Mint. Prove.</span>
+            <span className="text-xs">Learn. Prove. Mint.</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
